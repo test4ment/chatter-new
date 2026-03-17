4c036279d79eb26cd80ab1991eb5dd7a08990b79
test4me <nkaliczkij@yandex.ru>
2026-03-17 23:46:22 +0600

    msg meta to internal

--- Lib/Messaging/Messages/MessageMetadata.cs
+++ Lib/Messaging/Messages/MessageMetadata.cs
@@ -2,7 +2,7 @@ using System.Text.Json;
 
 namespace chatter_new.Messaging.Messages;
 
-public record MessageMetadata
+internal record MessageMetadata
 {
     public int ContentSize { get; init; }
     public bool TrackProgress { get; init; }
