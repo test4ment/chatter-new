fe68c6f55740ab0656d7d81ebd72ed9681f25325
test4me <nkaliczkij@yandex.ru>
2026-05-03 21:14:35 +0600

    chatter auth; identity doesnt deserialize correcrtly

---
+++ Tests/Authentification.cs
@@ -0,0 +1,43 @@
+using System.Security.Cryptography;
+using chatter_new_auth;
+using chatter_new.Messaging;
+
+namespace chatter_new_tests;
+
+public class Authentification
+{
+    [Fact]
+    public void CreateIdentityWithPassword()
+    {
+        var identity = new Identity();
+        var pw = "password".Encode();
+        var saved = identity.Serialize(pw);
+        // create identity with password
+
+        // encrypt some personal data
+        var somedata = "somedata".Encode();
+        var somedataEncrypted = identity.Encrypt(somedata);
+        // load identity with password
+        var loadedIdentity = Identity.FromJSON(saved, pw);
+        
+        // encryption still works
+        Assert.Equal(loadedIdentity.Encrypt(somedata), somedataEncrypted);
+        
+        // decrypt some personal data
+        var somedataDecrypted = loadedIdentity.Decrypt(somedataEncrypted);
+
+        Assert.Equal(somedata, somedataDecrypted);
+    }
+
+    [Fact]
+    public void PublicKeyAuthentity()
+    {
+        var identity = new Identity();
+        
+        
+        // identity provides public and private key
+        // share public
+        // receive encrypted data
+        // response with normal answer
+    }
+}
---
+++ chatter-new-auth/Identity.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using System.Text.Json;
+
+namespace chatter_new_auth;
+
+public class Identity
+{
+    private readonly RSA rsa;
+    public Identity() => rsa = RSA.Create();
+    private Identity(RSA rsa) => this.rsa = rsa;
+
+    public byte[] PublicKey => rsa.ExportRSAPublicKey();
+
+    public byte[] Encrypt(byte[] data) 
+        => rsa.Encrypt(data, RSAEncryptionPadding.OaepSHA256);
+    public byte[] Decrypt(byte[] data)
+        => rsa.Decrypt(data, RSAEncryptionPadding.OaepSHA256);
+
+    public string Serialize(byte[] password)
+    {
+        var encryptor = new UniversalEncryption(password);
+        rsa.ExportRSAPrivateKeyPem();
+        var @private = encryptor.Encrypt(rsa.ExportRSAPrivateKey());
+        var @public = encryptor.Encrypt(rsa.ExportRSAPublicKey());
+        // RSA.Create(new )
+        var rec = new IdentityJSON(@private, @public);
+        return JsonSerializer.Serialize(rec);
+    }
+
+    public static Identity FromJSON(string json, byte[] password)
+    {
+        var rec = JsonSerializer.Deserialize<IdentityJSON>(json)!;
+        var decryptor = new UniversalEncryption(password);
+        
+        var @private = decryptor.Decrypt(rec.privateKeyEnc);
+        var @public = decryptor.Decrypt(rec.publicKeyEnc);
+        
+        var rsa = RSA.Create();
+        rsa.ImportRSAPublicKey(@public, out _);
+        rsa.ImportRSAPrivateKey(@private, out _);
+        
+        return new Identity(rsa);
+    }
+    
+    internal record IdentityJSON(byte[] privateKeyEnc, byte[] publicKeyEnc);
+}
