d542a672292515adcf70d79dd9211302b5c7f126
test4me <nkaliczkij@yandex.ru>
2026-05-06 23:47:08 +0600

    multiserver app test

---
+++ chatter-server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Concurrent;
+using System.Net;
+using chatter_new.Messaging;
+using chatter_new.Messaging.Messages;
+using chatter_new.Messaging.Session;
+
+var sessionMaker = SocketConnection.ListenAndAwaitClients(new IPEndPoint(IPAddress.Any, 16777)).ConfigureAwait(false);
+var sessions = new ConcurrentDictionary<int, EncryptedSession>();
+var listenerTask = Task.Run(async () =>
+{
+    await foreach (var client in sessionMaker)
+    {
+        var newSess = await EncryptedSession.Create(client);
+        var id = sessions.Count;
+        newSess.OnReceive += (s, msg) =>
+        {
+            if (msg is TextMessage tmsg)
+            {
+                Console.WriteLine($"{id}: {tmsg.Text}");
+            }
+            else if (msg is SystemMessage sysmsg && sysmsg.Type == SystemMessage.SysMsgType.Left)
+            {
+                Console.WriteLine($"{id} sent {sysmsg.Type}");
+                if (!sessions.Remove(id, out _)) {
+                    Console.WriteLine($"[WARN]: {id} not found in sessions");
+                }
+            }
+            else {
+                Console.WriteLine($"{id}: <unsupported message>");
+                // TODO: add msg ignore mechanism
+            }
+        };
+        sessions[id] = newSess;
+    }
+});
+
+while (true)
+{
+    
+}
+
+listenerTask.Cancel
+Console.WriteLine("Hello, World!");
