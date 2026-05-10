05d277c4126a049ca691c466dc4df7cf0294b826
test4me <nkaliczkij@yandex.ru>
2026-05-11 00:48:21 +0600

    refac

--- Lib/Messaging/Encryption.cs
+++ Lib/Messaging/Encryption.cs
@@ -7,10 +7,10 @@ public class DHKeyExchange: IDisposable
     private Lazy<ECDiffieHellman> dh_inst = new(ECDiffieHellman.Create);
     public byte[] PublicKey => dh_inst.Value.PublicKey.ExportSubjectPublicKeyInfo();
 
-    public byte[] DerivePrivateKey(byte[] publicKey)
+    public byte[] DerivePrivateKey(byte[] remotePublicKey)
     {
         using var remote = ECDiffieHellman.Create();
-        remote.ImportSubjectPublicKeyInfo(publicKey, out _);
+        remote.ImportSubjectPublicKeyInfo(remotePublicKey, out _);
 
         return dh_inst.Value.DeriveKeyMaterial(remote.PublicKey);
     }
