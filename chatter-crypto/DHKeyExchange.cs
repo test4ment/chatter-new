f0c50a0eb11dbdfb23d2d69b464b42a2e86c5c5c
test4me <nkaliczkij@yandex.ru>
2026-05-11 19:06:34 +0600

    DFKeyExchange moved to crypto

--- chatter-crypto/DHKeyExchange.cs
+++ chatter-crypto/DHKeyExchange.cs
@@ -1,23 +1,23 @@
 using System.Security.Cryptography;
 
-namespace chatter_new.Messaging;
+namespace chatter_crypto;
 
 public class DHKeyExchange: IDisposable
 {
-    private Lazy<ECDiffieHellman> dh_inst = new(ECDiffieHellman.Create);
-    public byte[] PublicKey => dh_inst.Value.PublicKey.ExportSubjectPublicKeyInfo();
+    private ECDiffieHellman dh_inst = ECDiffieHellman.Create(ECCurve.NamedCurves.nistP521);
+    public byte[] PublicKey => dh_inst.PublicKey.ExportSubjectPublicKeyInfo();
 
     public byte[] DerivePrivateKey(byte[] remotePublicKey)
     {
         using var remote = ECDiffieHellman.Create();
         remote.ImportSubjectPublicKeyInfo(remotePublicKey, out _);
 
-        return dh_inst.Value.DeriveKeyMaterial(remote.PublicKey);
+        return dh_inst.DeriveKeyMaterial(remote.PublicKey);
     }
     public void Dispose()
     {
-        dh_inst.Value.Dispose();
+        GC.SuppressFinalize(this);
+        dh_inst.Dispose();
         dh_inst = null!;
     }
 }
-
